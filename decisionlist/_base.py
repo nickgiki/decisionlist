3c5f5d352c88ca4d834d894c645c2e90f8e7ab93
unknown <nickgikizas@hotmail.com>
2022-11-07 23:25:57 +0200

    add helper functions

--- decisionlist/_base.py
+++ decisionlist/_base.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.tree import _tree
+from sklearn.tree._classes import DecisionTreeClassifier, DecisionTreeRegressor
 
 
 def is_numeric(vector: np.ndarray):
@@ -11,15 +12,19 @@ def is_numeric(vector: np.ndarray):
     except:
         return False
 
-def mine_tree_rules(tree, feature_names = None, class_names = None, sign_digits=3):
+
+def mine_tree_rules(tree, feature_names=None, class_names=None, sign_digits=3):
     """copied and modified from:
     https://mljar.com/blog/extract-rules-decision-tree/
     """
     tree_ = tree.tree_
-    
+
     if feature_names == None:
-        feature_names = [f'__col{i}' for i in range(tree.n_features_in_)]
-    
+        feature_names = [f"__col{i}" for i in range(tree.n_features_in_)]
+
+    if class_names is None and isinstance(tree, DecisionTreeClassifier):
+        class_names = tree.classes_
+
     feature_name = [
         feature_names[i] if i != _tree.TREE_UNDEFINED else "undefined!"
         for i in tree_.feature
@@ -27,7 +32,7 @@ def mine_tree_rules(tree, feature_names = None, class_names = None, sign_digits=
 
     paths = []
     path = []
-    
+
     def recurse(node, path, paths):
 
         if tree_.feature[node] != _tree.TREE_UNDEFINED:
@@ -57,15 +62,20 @@ def mine_tree_rules(tree, feature_names = None, class_names = None, sign_digits=
             rule += [str(p)]
         rule_list += [tuple(rule)]
 
-        if class_names is None:
-            # classification
+        if isinstance(tree, DecisionTreeRegressor):
+            # regression
+
             rule_list += [str(np.round(path[-1][0][0][0], 3))]  # class
         else:
-            # regression
-            classes = path[-1][0][0]
-            l = np.argmax(classes)
+            # classification
+
+            class_counts = path[-1][0][0]
+            l = np.argmax(class_counts)
+
+            rule_list += [class_names[l]]
+            rule_list += [class_counts.tolist()]
             rule_list += [
-                np.round(classes[l] / np.sum(classes), sign_digits)
+                np.round(1.0 * class_counts[l] / np.sum(class_counts), sign_digits)
             ]  # confidence
         rule_list += [path[-1][1]]  # support
         rules += [tuple(rule_list)]
@@ -74,6 +84,7 @@ def mine_tree_rules(tree, feature_names = None, class_names = None, sign_digits=
 
 
 def beautify_rules(rules, one_hot_encoder):
+
     """Beautifies and makes rules more concise"""
     rules_c = []
 
@@ -96,8 +107,8 @@ def beautify_rules(rules, one_hot_encoder):
         conditions_array = np.array([features, inequalities, values]).T
 
         unique_features = np.unique(features, return_counts=True)
-        
-        #if there are features that exist more than once in the rule conditions
+
+        # if there are features that exist more than once in the rule conditions
         if unique_features[1].max() > 1:
             redundant_indices = []
             for feature in unique_features[0][unique_features[1] > 1]:
@@ -105,13 +116,13 @@ def beautify_rules(rules, one_hot_encoder):
                     conditions_array[:, 0] == feature
                 ]
                 unique_ineq = np.unique(conditions_filtered[:, 1], return_counts=True)
-                
-                #if there are more than one occurences for an inequality
+
+                # if there are more than one occurences for an inequality
                 if unique_ineq[1].max() > 1:
                     for ineq in unique_ineq[0][unique_ineq[1] > 1]:
                         if ineq == ">":
-                            
-                            #keep only the maximum
+
+                            # keep only the maximum
                             max_val = (
                                 conditions_filtered[
                                     conditions_filtered[:, 1] == ineq, 2
@@ -125,9 +136,9 @@ def beautify_rules(rules, one_hot_encoder):
                                 & (conditions_array[:, 1] == ineq)
                                 & (conditions_array[:, 2].astype(float) < max_val)
                             )[0].tolist()
-                        
+
                         else:
-                            #keep only the minimum
+                            # keep only the minimum
                             min_val = (
                                 conditions_filtered[
                                     conditions_filtered[:, 1] == ineq, 2
@@ -141,7 +152,6 @@ def beautify_rules(rules, one_hot_encoder):
                                 & (conditions_array[:, 1] == ineq)
                                 & (conditions_array[:, 2].astype(float) > min_val)
                             )[0].tolist()
-
 
             rules_c += [
                 tuple(
@@ -160,3 +170,30 @@ def beautify_rules(rules, one_hot_encoder):
         else:
             rules_c += [rule]
     return rules_c
+
+
+def get_rules_from_forest(forest, min_confidence, min_support, beautify=True):
+    """extracts rules from a random forest"""
+    all_rules = []
+
+    for i in range(len(forest.estimators_)):
+        if beautify:
+            all_rules += beautify_rules(mine_tree_rules(forest.estimators_[i]), None)
+
+        else:
+            all_rules += mine_tree_rules(forest.estimators_[i], None, forest.classes_)
+    # return [r for r in all_rules if r[-2] >= min_confidence and r[-1] >= min_support]
+    return all_rules
+
+
+def sort_rules(rules):
+    """Sorts rules by confidence then support"""
+    rule_array = np.array(
+        [[i, rules[i][-2], rules[i][-1], len(rules[i][0])] for i in range(len(rules))]
+    )
+
+    # sort by confidence then support
+    sorted_idx = rule_array[
+        np.lexsort((rule_array[:, 1], rule_array[:, 2], rule_array[:, 3]))[::-1]
+    ][:, 0].astype(int)
+    return [rules[i] for i in sorted_idx]
