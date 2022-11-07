8470d749680636dcf589af33e6d61f75290939ef
unknown <nickgikizas@hotmail.com>
2022-11-07 17:06:31 +0200

    fixed bugs

--- decisionlist/_base.py
+++ decisionlist/_base.py
@@ -2,11 +2,24 @@ import numpy as np
 from sklearn.tree import _tree
 
 
-def mine_tree_rules(tree, feature_names, class_names, sign_digits=3):
+def is_numeric(vector: np.ndarray):
+    """Checks if a vector is numeric"""
+    vector = np.unique(vector[vector != None])
+    try:
+        vector.astype(np.float64)
+        return True
+    except:
+        return False
+
+def mine_tree_rules(tree, feature_names = None, class_names = None, sign_digits=3):
     """copied and modified from:
     https://mljar.com/blog/extract-rules-decision-tree/
     """
     tree_ = tree.tree_
+    
+    if feature_names == None:
+        feature_names = [f'__col{i}' for i in range(tree.n_features_in_)]
+    
     feature_name = [
         feature_names[i] if i != _tree.TREE_UNDEFINED else "undefined!"
         for i in tree_.feature
@@ -14,7 +27,7 @@ def mine_tree_rules(tree, feature_names, class_names, sign_digits=3):
 
     paths = []
     path = []
-
+    
     def recurse(node, path, paths):
 
         if tree_.feature[node] != _tree.TREE_UNDEFINED:
@@ -83,7 +96,8 @@ def beautify_rules(rules, one_hot_encoder):
         conditions_array = np.array([features, inequalities, values]).T
 
         unique_features = np.unique(features, return_counts=True)
-
+        
+        #if there are features that exist more than once in the rule conditions
         if unique_features[1].max() > 1:
             redundant_indices = []
             for feature in unique_features[0][unique_features[1] > 1]:
@@ -91,37 +105,41 @@ def beautify_rules(rules, one_hot_encoder):
                     conditions_array[:, 0] == feature
                 ]
                 unique_ineq = np.unique(conditions_filtered[:, 1], return_counts=True)
+                
+                #if there are more than one occurences for an inequality
                 if unique_ineq[1].max() > 1:
                     for ineq in unique_ineq[0][unique_ineq[1] > 1]:
                         if ineq == ">":
-
+                            
+                            #keep only the maximum
                             max_val = (
                                 conditions_filtered[
                                     conditions_filtered[:, 1] == ineq, 2
                                 ]
-                                .astype(int)
+                                .astype(float)
                                 .max()
                             )
 
                             redundant_indices += np.where(
                                 (conditions_array[:, 0] == feature)
                                 & (conditions_array[:, 1] == ineq)
-                                & (conditions_array[:, 2].astype(int) < max_val)
+                                & (conditions_array[:, 2].astype(float) < max_val)
                             )[0].tolist()
                         
                         else:
+                            #keep only the minimum
                             min_val = (
                                 conditions_filtered[
                                     conditions_filtered[:, 1] == ineq, 2
                                 ]
-                                .astype(int)
+                                .astype(float)
                                 .min()
                             )
 
                             redundant_indices += np.where(
                                 (conditions_array[:, 0] == feature)
                                 & (conditions_array[:, 1] == ineq)
-                                & (conditions_array[:, 2].astype(int) > min_val)
+                                & (conditions_array[:, 2].astype(float) > min_val)
                             )[0].tolist()
 
 
