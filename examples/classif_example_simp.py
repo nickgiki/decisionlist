4deaf047d7dd2dde2e720ed0bd4b4c57ee6f546f
Nikos Gkikizas <nikos.gkikizas@satalia.com>
2022-11-09 11:24:58 +0200

    fix bugs in examples

--- examples/classif_example_simp.py
+++ examples/classif_example_simp.py
@@ -1,8 +1,8 @@
 from decisionlist.estimators import DecisionListClassifier
 from decisionlist._base import (
     is_numeric,
-    sort_rules_c,
-    get_rules_from_forest_c,
+    sort_rules,
+    get_rules_from_forest,
     get_num_rules,
     num_rule_ind,
 )
@@ -12,22 +12,23 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-X, y = make_classification(1000, n_features=10)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
-
 results = []
 
 
 np.random.seed(1)
 
 for i in range(50):
+    
+    #create data
+    X, y = make_classification(1000, n_features=10)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+    
     # fit the dl classifier
-    dlc = DecisionListClassifier()
+    dlc = DecisionListClassifier(max_rule_length=3)
     dlc.fit(X_train, y_train)
 
     # fit the regressor
-    dtc = DecisionTreeClassifier()
+    dtc = DecisionTreeClassifier(max_depth=3)
     dtc.fit(X_train, y_train)
 
     results += [dlc.score(X_test, y_test) / dtc.score(X_test, y_test) - 1]
